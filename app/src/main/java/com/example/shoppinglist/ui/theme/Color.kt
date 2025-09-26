a575d58eef75e4276b2d13b6f04d4ec41f3746bb
Naisya Harjimen <nsyaharjimen@gmail.com>
2025-09-26 09:00:45 +0700

    Initial commit

--- app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
@@ -2,30 +2,29 @@ package com.example.shoppinglist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val BlueLight = Color(0xFF2196F3)
+val BlueDark = Color(0xFF1976D2)
+val BlueOn = Color(0xFFFFFFFF)
 
+val BlueBackground = Color(0xFFF5F5F5)
+val BlueOnBackground = Color(0xFF1C1B1F)
+val BlueSurface = Color(0xFFFFFFFF)
+val BlueOnSurface = Color(0xFF1C1B1F)
 
-val RedLight = Color(0xFFF44336)
-val RedDark = Color(0xFFD32F2F)
-val RedOn = Color(0xFFFFFFFF)
-val RedBackground = Color(0xFFF5F5F5)
-val RedOnBackground = Color(0xFF1C1B1F)
-val RedSurface = Color(0xFFFFFFFF)
-val RedOnSurface = Color(0xFF1C1B1F)
+val BlueSurfaceDark = Color(0xFF2B2930)
+val BlueOnSurfaceDark = Color(0xFFE6E1E5)
 
-val RedSurfaceDark = Color(0xFF2B2930)
-val RedOnSurfaceDark = Color(0xFFE6E1E5)
+val BlueSecondary = Color(0xFF03A9F4)
+val BlueSecondaryContainer = Color(0xFFB3E5FC)
+val BlueOnSecondaryContainer = Color(0xFF002F44)
 
-val RedSecondary = Color(0xFFFF5722)
-val RedSecondaryContainer = Color(0xFFFFE0DB)
-val RedOnSecondaryContainer = Color(0xFF8C2318)
-
-val RedPrimaryContainer = Color(0xFFFFDAD6)
-val RedOnPrimaryContainer = Color(0xFF410E0B)
+val BluePrimaryContainer = Color(0xFFBBDEFB)
+val BlueOnPrimaryContainer = Color(0xFF0D47A1)
 
 // Warna untuk dark mode
-val RedDarkBackground = Color(0xFF121212)
-val RedDarkOnBackground = Color(0xFFE6E1E5)
-val RedDarkPrimaryContainer = Color(0xFF93000A)
-val RedDarkOnPrimaryContainer = Color(0xFFFFDAD6)
-val RedDarkSecondaryContainer = Color(0xFF8C2318)
-val RedDarkOnSecondaryContainer = Color(0xFFFFDAD6)
+val BlueDarkBackground = Color(0xFF121212)
+val BlueDarkOnBackground = Color(0xFFE6E1E5)
+val BlueDarkPrimaryContainer = Color(0xFF0D47A1)
+val BlueDarkOnPrimaryContainer = Color(0xFFBBDEFB)
+val BlueDarkSecondaryContainer = Color(0xFF004B5F)
+val BlueDarkOnSecondaryContainer = Color(0xFFB3E5FC)
